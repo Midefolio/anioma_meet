69fccb059bbaf1d2034d0f3ed6417e454abebb2e
Midefolio <ayomidebabatundeolosho@gmail.com>
2024-12-02 03:40:53 +0100

    jkj

---
+++ src/component/footer.tsx
@@ -0,0 +1,58 @@
+const MyFooter = () => {
+    return ( <>
+   <div className="bg-color-code-2 my-mother my-bottom-50">
+    <div className="my-col-10 xs-10 xs-off-1 md-10 md-off-1 off-1 down-5 md-down-5 xs-down-5">
+     <div className="my-mother">
+      <div className="my-col-4 xs-12 md-12">
+      <div className="folda-logo"></div>  
+      <div className="my-mother down-5">
+        <span className="px13 alice xs-px13 md-px20 lugfaBold upper-case">ContACT US</span>
+        <div className="my-mother down-8 xs-down-5 md-down-3 hidden-ls"><span className="px12 xs-px12 md-px20 lower-case alice"><i className="fas fa-envelope color-code-1"></i> CHECKFOLDA@GMAIL.COM</span></div>
+        <div className="my-mother hidden-ls down-5 xs-down-2 md-down-3"><span className="px12 xs-px12 md-px20 lower-case alice"><i className="fas fa-phone color-code-1"></i> +234-707-752-0079 </span></div>
+        <div className="my-mother hidden-ls down-5 xs-down-2 md-down-3"><span className="px12 xs-px12 md-px20 lower-case alice"><i className="fas fa-map-marker-alt color-code-1"></i> Landmark House 52-54, Isaac John Street GRA-Ikeja, Lagos,100271,NGA </span></div>
+      </div>
+      </div>
+      <div className="my-col-4 xs-12 md-down-10 xs-down-10 md-12">
+       <div className="px13 alice down-4 xs-px13 md-px20 lugfaBold upper-case">Get the freshest news from us</div>
+        <div className="my-mother down-6 xs-down-3 md-down-3">
+        <div className="px13 alice xs-px13 gap-elements md-px20">
+          <input type="text" placeholder="Type your email" className="my-col-7 xs-px12 md-px13 input px9 InterLight bg-white rad-10" />
+          <span className="white px10 xs-px12 centered-align pd-10 bg-color-code-1 rad-10">Subscribe</span>
+        </div>
+      </div>
+      </div>
+      <div className="my-col-4 xs-12 md-down-8 xs-down-10 md-12">
+      <div className="px13 alice down-4 ls-right xs-px13 md-px20 lugfaBold upper-case">Socials</div>
+        <div className="my-mother ls-right down-6 md-down-3 xs-down-3">
+        <div className="px13 alice xs-px13 ls-right xs-down-5 gap-eements md-px20">
+          <a href="https://www.facebook.com/share/1E4kn5J2Be/" className="pd-5 white"><i className="fab fa-facebook"></i></a>
+          <a href="https://x.com/foldaapp" className="pd-5 white mg-5"><i className="fab fa-twitter"></i></a>
+          <a href="https://instagram.com/checkfolda" className="pd-5 white mg-5"><i className="fab fa-instagram"></i></a>
+          <a href="https://www.linkedin.com/company/foldaapp/" className="pd-5 white mg-5"><i className="fab fa-linkedin"></i></a>
+        </div>
+        <div className="my-mother down-3 white xs-down-6">@checkfolda</div>
+        <div className="my-mother hidden-xs hidden-md hidden-ls down-8 xs-down-3 md-down-3"><span className="px12 xs-px13 md-px20 hidden-xs lower-case alice"><i className="fas fa-map-marker-alt color-code-1"></i> Landmark House 52-54, Isaac John Street GRA-Ikeja, Lagos,100271,NGA  </span></div>
+      </div>
+      </div>
+     </div>
+     <div className="my-mother down-5 centered-align gap-elements hidden-md hidden-xs  space-50">
+      <div className="my-col-4">
+        <div className="my-mother gap-elements"><span className="px12 xs-px13 md-px20 hidden-xs lower-case alice"><i className="fas fa-phone color-code-1"></i> CHECKFOLDA@GMAIL.COM </span></div>
+      </div>
+      <div className="my-col-4">
+        <div className="my-mother gap-elements"><span className="px12 xs-px13 md-px20 hidden-xs lower-case alice"><i className="fas fa-phone color-code-1"></i> +234-707-752-0079 </span></div>
+      </div>
+      <div className="my-col-4">
+      <div className="my-mother gap-elements"><span className="px12 xs-px12 md-px20 lower-case alice"><i className="fas fa-map-marker-alt color-code-1"></i> Landmark House 52-54, Isaac John Street GRA-Ikeja, Lagos,100271,NGA </span></div>
+      </div>
+     
+     </div>
+     <div className="my-mother bd-bottom-bold xs-down-5 md-down-5 down-5"></div>
+     <div className="centered down-5 my-mother xs-down-10 md-down-5  xs-px9 md-px13 alice px10"><span>© 2024 Folda. All rights reserved. Privacy Policy | Terms of Service</span></div>
+    </div>
+   </div>
+    
+    </> );
+}
+ 
+export default MyFooter;
--- src/screens/page.tsx
+++ src/screens/page.tsx
@@ -178,7 +178,7 @@ const Home = () => {
       </div>
 
 
-
+ <Footer
     </>
   );
 };
